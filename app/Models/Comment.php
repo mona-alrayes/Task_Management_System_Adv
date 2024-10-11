bfdf795e0ea391974cf97e05c1f9b05e071d0b95
Mona <moon.2060@gmail.com>
2024-10-11 19:20:53 +0300

    'adding morph relationship of comments'

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -16,11 +16,24 @@ class Comment extends Model
      * @var array<int, string>
      */
     protected $fillable = [
+
         'comment'
     ];
 
-    public function commentable(){
-        
+    /**
+     * Get the parent model (task or any other model) that this comment belongs to.
+     * 
+     * This method refers to the polymorphic relationship, allowing a comment 
+     * to be associated with more than one type of model (e.g., Task, Post, etc.).
+     * 
+     * In a polymorphic relationship, the `comments` table contains two columns:
+     * `commentable_id` (the ID of the related model) and `commentable_type` 
+     * (the class name of the related model, such as `App\Models\Task`).
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function commentable()
+    {
         return $this->morphTo();
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -16,7 +16,14 @@ class Task extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        // Add your fillable fields here
+        'type',
+        'title',
+        'description',
+        'priority',
+        'status',
+        'due_date',
+        'assigned_to',
+
     ];
 
     /**
@@ -26,21 +33,35 @@ class Task extends Model
      */
     protected $dates = ['deleted_at'];
 
+
     /**
-    * The attributes that should be hidden for serialization.
-    *
-    * @var array<int, string>
-    */
-    protected $hidden = [
-
-     ];
-
-     /**
      * The attributes that should be cast.
      *
      * @var array<string, string>
      */
-     protected $casts = [
+    protected $casts = [
+        'due_date' => 'date',
+    ];
 
-      ];
+    /**
+     * Get all comments for the model (Task, Post, etc.) using a polymorphic relationship.
+     * 
+     * This method defines a one-to-many polymorphic relationship, allowing the model 
+     * to have multiple comments. The comments are stored in a shared `comments` table 
+     * and can be associated with various models (e.g., Task, Post) through the 
+     * `commentable_type` and `commentable_id` fields.
+     * 
+     * The `morphMany` method indicates that this model can have multiple comments, 
+     * and it retrieves all comments associated with this specific model instance.
+     * 
+     * Example usage:
+     * $task = Task::find(1);
+     * $comments = $task->comments;
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
