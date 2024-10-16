79a341a232d7e401610d22b889b90d81ed060dfd
Mona <moon.2060@gmail.com>
2024-10-16 19:28:32 +0300

    'update ErrorLogController documenation'

--- app/Http/Controllers/ErrorLogController.php
+++ app/Http/Controllers/ErrorLogController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\ErrorLog;
-use GrahamCampbell\ResultType\Success;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ErrorLogController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the error logs.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
     {
-        // Retrieve all error logs
+        // Retrieve all error logs, ordered by created_at in descending order
         $errorLogs = ErrorLog::orderBy('created_at', 'desc')->paginate(10);
 
         // Return the error logs paginated
-        return self::paginated($errorLogs , 'errors retrevied successfully', 200);
+        return self::paginated($errorLogs, 'Errors retrieved successfully', 200);
     }
 }
